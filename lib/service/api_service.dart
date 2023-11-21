42e2978daf3c630301696ad9555f4184b5fe3b70
Suyasha Nepal <nepalsuyasha@gmail.com>
2023-11-21 22:17:09 +0545

    changes

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -5,14 +5,14 @@ import 'package:flutterpersonal/constants/api.dart';
 import 'package:flutterpersonal/shared/client_provider.dart';
 
 
-final apiService= Provider((ref) => ApiService(ref.watch(clientProvider)));
+final apiService= FutureProvider((ref) => ApiService(ref.watch(clientProvider)).getMovieCategory());
 
 class ApiService {
 
 final Dio dio;
 ApiService(this.dio);
 
-  Future<List<Movie>> getMealData () async{
+  Future<List<Movie>> getMovieCategory () async{
     try{
       final response= await dio.get(Api.getPopular);
       return (response.data['results'] as List).map((e) => Movie.fromJson(e)).toList();
